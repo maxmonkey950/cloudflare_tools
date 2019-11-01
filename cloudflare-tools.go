548602fee4056bbe16ad3526417bbf340f55cfc9
maxmonkey950 <46515596+maxmonkey950@users.noreply.github.com>
2019-11-01 10:47:13 +0800

    update main file
    add configure postgresql interface.

--- cloudflare-tools.go
+++ cloudflare-tools.go
@@ -6,60 +6,89 @@ import (
 	"flag"
 	"fmt"
 	cloudflare "github.com/cloudflare/cloudflare-go"
+	"go_cf_postgres"
 	"log"
+	"os"
+	"path"
+	"runtime"
 	"strconv"
+	"strings"
 )
 
+func GetCurFilename() string {
+	_, fulleFilename, _, _ := runtime.Caller(0)
+	var filenameWithSuffix string
+	filenameWithSuffix = path.Base(fulleFilename)
+	var fileSuffix string
+	fileSuffix = path.Ext(filenameWithSuffix)
+
+	var filenameOnly string
+	filenameOnly = strings.TrimSuffix(filenameWithSuffix, fileSuffix)
+
+	return filenameOnly
+}
+
+var email string
+var api_key string
+
 func main() {
-	var email string
-	var api_key string
-	flag.StringVar(&email, "e", "", "default is nil")
-	flag.StringVar(&api_key, "a", "", "default is nil")
+	flag.StringVar(&email, "e", "", "email, default is nil")
+	flag.StringVar(&api_key, "a", "", "api_key, default is nil")
 	flag.Parse()
 	fmt.Println(api_key, email)
+	var filenameOnly string = GetCurFilename()
+	var logFilename string = filenameOnly + ".log"
+	logFile, err := os.OpenFile(logFilename, os.O_RDWR|os.O_CREATE, 0777)
+	logger := log.New(logFile, "", log.Ldate|log.Ltime|log.Lshortfile)
+	if err != nil {
+		fmt.Printf("open file error=%s\r\n", err.Error())
+		os.Exit(-1)
+	}
+	defer logFile.Close()
+	//api, err := cloudflare.New("xxx", "xxx@gmail.com")
+	//api, err := cloudflare.New("xxx", "xxx@xxx.com")
 	api, err := cloudflare.New(api_key, email)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 		return
-	}
-	// Fetch all zones available to this user.
-	zones, err := api.ListZones()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	for _, z := range zones {
-		//fmt.Println(z.Name)
-		zoneID, err := api.ZoneIDByName(z.Name)
-		//ct := "www." + z.Name
-		//fmt.Println(ct)
-		localhost := cloudflare.DNSRecord{Content: "35.203.158.174", Name: ("www." + z.Name)}
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		recs, err := api.DNSRecords(zoneID, localhost)
+	} else {
+		// Fetch all zones available to this user.
+		zones, err := api.ListZones()
 		if err != nil {
 			log.Fatal(err)
-			return
 		} else {
-			for _, r := range recs {
-				if strconv.FormatBool(r.Proxied) == "false" {
-					return
+			for _, z := range zones {
+				fmt.Println(z.Name)
+				zoneID, err := api.ZoneIDByName(z.Name)
+				if err != nil {
+					fmt.Println(err)
+				}
+				localhost := cloudflare.DNSRecord{Content: "23.227.38.32", Name: ("letter." + z.Name)}
+				recs, err := api.DNSRecords(zoneID, localhost)
+				if err != nil {
+					log.Fatal(err)
+					logger.Printf("%+v not shopify website", z.Name)
 				} else {
-					recsrule, err := api.ListPageRules(zoneID)
-					if err != nil {
-						log.Fatal(err)
-						return
-					} else {
-						for _, v := range recsrule {
-							if v.Status == "active" {
-								m := v.Actions[0].Value.(map[string]interface{})
-								fmt.Printf("%+v %+v %+v %+v\n", z.Name, m["status_code"], m["url"], v.Status)
-								//fmt.Println(v.Actions[0].Value)
-								//for _, vasd := range m {
-								//	fmt.Printf("%+v %+v %+v\n", z.Name, vasd, m["url"])
-								//}
+					for _, r := range recs {
+						if strconv.FormatBool(r.Proxied) == "false" {
+							//fmt.Println("no proxy!")
+							logger.Printf("%+v has no proxy", z.Name)
+						} else {
+							logger.Printf("%+v\n having proxy", z.Name)
+							recsrule, err := api.ListPageRules(zoneID)
+							if err != nil {
+								log.Fatal(err)
+								return
+							} else {
+								for _, v := range recsrule {
+									if v.Status == "active" {
+										m := v.Actions[0].Value.(map[string]interface{})
+										logger.Println(z.Name, m["status_code"], m["url"], v.Status)
+										ms := m["url"].(string)
+										go_cf_postgres.Inst(z.Name, ms)
+										fmt.Printf("%+v %+v %+v %+v\n", z.Name, m["status_code"], m["url"], v.Status)
+									}
+								}
 							}
 						}
 					}
